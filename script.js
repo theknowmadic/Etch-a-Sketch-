6d7d0a0741977409e6fac732a998932ddf0e9b4a
theknowmad <127548786+theknowmadic@users.noreply.github.com>
2023-04-03 09:17:08 -0400

    Adding eventListeners for 'hover over' effect

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const grid = document.querySelector('.container')
+const container = document.querySelector('.container')
 
+// Function that uses a for-loop to create a 16x16 grid
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows)
-}
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
