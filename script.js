0b075cc53d6b5a2c52039fd91f99c8f7692b6db2
theknowmad <127548786+theknowmadic@users.noreply.github.com>
2023-04-03 13:39:33 -0400

    Refactoring

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const container = document.querySelector('.container')
-
+const container = document.querySelector('.container');
+const button = document.querySelector('.popup');
 
 
 // Function that uses a for-loop to create a 16x16 grid
-function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
+function makeRows(userSize) {
+    container.style.setProperty('--grid-rows', userSize);
+    container.style.setProperty('--grid-cols', userSize);
+    for (c = 0; c < (userSize * userSize); c++) {
         const cell = document.createElement("div");
         cell.addEventListener('mouseover', () => { 
             cell.style.backgroundColor = 'black'
@@ -15,4 +15,14 @@ function makeRows(rows, cols) {
     };
 };
 
-makeRows(16, 16);
+button.addEventListener('click', () => {
+    let userSize = Number(prompt("Choose the amount of boxes you want", "1-100"))
+
+    while (userSize > 100) {
+        userSize = Number(prompt("Pick a smaller number less than 100", "1-100"))
+    }
+
+    makeRows(userSize)
+})
+
+makeRows()
