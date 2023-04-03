d9afda6fe34e9c04e8dc61a31baf49963ed2a410
theknowmad <127548786+theknowmadic@users.noreply.github.com>
2023-04-03 16:19:09 -0400

    Project complete: Refactoring

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
-const container = document.querySelector('.container');
 const button = document.querySelector('.popup');
+const container = document.querySelector('.container')
 
 
-// Function that uses a for-loop to create a 16x16 grid
-function makeRows(userSize) {
-    container.style.setProperty('--grid-rows', userSize);
-    container.style.setProperty('--grid-cols', userSize);
-    for (c = 0; c < (userSize * userSize); c++) {
-        const cell = document.createElement("div");
-        cell.addEventListener('mouseover', () => { 
-            cell.style.backgroundColor = 'black'
+
+// Function that uses two for-loops to create a grid
+const makeRows = (grid) => {
+    const wrapper = document.createElement('div')
+    wrapper.classList.add('wrapper')
+
+    for (let i = 0; i < grid; i++) {
+        const row = document.createElement('div')
+        row.classList.add('grid-row')
+
+    for (let j = 0; j < grid; j++) {
+        const widthAndHeight = 960 / grid
+        const gridBox = document.createElement('div')
+        gridBox.classList.add('grid-box')
+        gridBox.style.width = `${widthAndHeight}px`
+        gridBox.style.height = `${widthAndHeight}px`
+        // Event listener
+        gridBox.addEventListener('mouseover', () => { 
+            gridBox.style.backgroundColor = 'black'
         })
-        container.appendChild(cell).className = "grid-item";
-    };
-};
+        row.appendChild(gridBox)
+         }
+         wrapper.appendChild(row)
+    }
+    container.appendChild(wrapper)
+}
+
+makeRows()
+
 
 button.addEventListener('click', () => {
     let userSize = Number(prompt("Choose the amount of boxes you want", "1-100"))
 
     while (userSize > 100) {
-        userSize = Number(prompt("Pick a smaller number less than 100", "1-100"))
+        userSize = Number(prompt("Ops! Pick a smaller number less than 100", "1-100"))
     }
 
+    const wrapper = document.querySelector('.wrapper')
+    wrapper.remove()
     makeRows(userSize)
 })
 
-makeRows()
+
